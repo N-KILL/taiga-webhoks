e3eb454622e1f422b8bafd2052389142e58d58f1
Esteban Ficetti <ficettiesteban@gmail.com>
2023-11-03 12:22:18 -0300

    added webhooks rendering & serverpod password

--- taiga_consumer_server/lib/src/web/routes/root.dart
+++ taiga_consumer_server/lib/src/web/routes/root.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:taiga_consumer_server/src/web/widgets/default_page_widget.dart';
@@ -6,8 +7,8 @@ import 'package:serverpod/serverpod.dart';
 class RouteRoot extends WidgetRoute {
   @override
   Future<Widget> build(Session session, HttpRequest request) async {
-    final body = request.session.values;
-    print(body);
-    return DefaultPageWidget();
+    final decodedBody = await utf8.decodeStream(request);
+    final body = json.decode(decodedBody);
+    return WebHooksView(webhookData: body);
   }
 }
--- taiga_consumer_server/lib/src/web/widgets/default_page_widget.dart
+++ taiga_consumer_server/lib/src/web/widgets/default_page_widget.dart
@@ -1,10 +1,22 @@
 import 'package:serverpod/serverpod.dart';
 
 class DefaultPageWidget extends Widget {
-  DefaultPageWidget() : super(name: 'default') {
+  final Map<String, dynamic> webhookData;
+
+  DefaultPageWidget({required this.webhookData}) : super(name: 'default') {
     values = {
       'served': DateTime.now(),
-      'runmode': Serverpod.instance!.runMode,
+      'runmode': webhookData.toString(),
+    };
+  }
+}
+
+class WebHooksView extends Widget {
+  final Map<String, dynamic> webhookData;
+
+  WebHooksView({required this.webhookData}) : super(name: 'webhooks') {
+    values = {
+      'data': webhookData.toString(),
     };
   }
 }
