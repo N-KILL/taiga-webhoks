0613e8d3d8d1ab1cac41d7bd5cc77cf059d1be3b
NonKill <69329781+N-KILL@users.noreply.github.com>
2023-11-10 09:53:56 -0300

    Try map on root function

--- taiga_consumer_server/lib/src/web/routes/root.dart
+++ taiga_consumer_server/lib/src/web/routes/root.dart
@@ -3,15 +3,25 @@ import 'dart:io';
 
 import 'package:taiga_consumer_server/src/web/widgets/default_page_widget.dart';
 import 'package:serverpod/serverpod.dart';
+import 'package:taiga_rest_models/taiga_rest_models.dart';
 
 class RouteRoot extends WidgetRoute {
   @override
   Future<Widget> build(Session session, HttpRequest request) async {
     final decodedBody = await utf8.decodeStream(request);
     final body = json.decode(decodedBody);
-    print('Webhook received (Request): $request');
-    print('Webhook received (DecodedBody): $decodedBody');
-    print('Webhook received (Body, JSON FORMAT): $body');
+    print('Webhook received:');
+    print('DecodedBody: $decodedBody');
+    print('Body as JSON FORMAT: $body');
+
+    final bodyMap = Issue(
+        data: body.data,
+        type: body.type,
+        action: body.action,
+        date: body.date,
+        by: body.by);
+        
+    print(bodyMap);
     return WebHooksView(webhookData: body);
   }
 }
