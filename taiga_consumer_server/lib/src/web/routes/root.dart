38e37f5acd73c35ee916e8a822e35b86789e8b4f
NonKill <69329781+N-KILL@users.noreply.github.com>
2023-11-13 14:30:13 -0300

    Print more data

--- taiga_consumer_server/lib/src/web/routes/root.dart
+++ taiga_consumer_server/lib/src/web/routes/root.dart
@@ -16,16 +16,16 @@ class RouteRoot extends WidgetRoute {
     print('This is jsonData: $jsonData');
     final model = TaigaPayload.fromJson(jsonData);
     print('This is model: $model');
-    // print('By Id: ${model.by.id}');
-    // print('Deleted By: ${model.by.fullName}');
-    // print('Date.id: ${model.data.id}');
-    // print('Date.ref: ${model.data.ref}');
-    // print('Date.createdDate: ${model.data.createdDate}');
-    // print('Date.permalink: ${model.data.permalink}');
-    // print('Date.ProjectName: ${model.data.project.name}');
-    // print('Date.TypeId: ${model.data.type.id}');
-    // print('Date.Type: ${model.data.type.color}');
-    // print('Date.TypeName: ${model.data.type.name}');
+    print('By Id: ${model.by.id}');
+    print('Deleted By: ${model.by.fullName}');
+    print('Date.id: ${model.data.id}');
+    print('Date.ref: ${model.data.ref}');
+    print('Date.createdDate: ${model.data.createdDate}');
+    print('Date.permalink: ${model.data.permalink}');
+    print('Date.ProjectName: ${model.data.project.name}');
+    print('Date.TypeId: ${model.data.type.id}');
+    print('Date.Type: ${model.data.type.color}');
+    print('Date.TypeName: ${model.data.type.name}');
     return WebHooksView(webhookData: body);
   }
 }
