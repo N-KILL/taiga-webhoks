e9aa4825eb03edb0c7b326f3be957126a5a33e47
Ignacio Cardozo <club_dog2@hotmail.com>
2024-01-24 12:00:25 -0300

    Verify if this break

--- taiga_consumer_client/lib/src/protocol/protocol.dart
+++ taiga_consumer_client/lib/src/protocol/protocol.dart
@@ -10,26 +10,6 @@
 library protocol; // ignore_for_file: no_leading_underscores_for_library_prefixes
 
 import 'package:serverpod_client/serverpod_client.dart' as _i1;
-import 'protocol/error_enum.dart' as _i2;
-import 'protocol/exception.dart' as _i3;
-import 'protocol/taiga/taiga_job_commentaries.dart' as _i4;
-import 'protocol/taiga/taiga_job_updates.dart' as _i5;
-import 'protocol/taiga/taiga_jobs.dart' as _i6;
-import 'protocol/taiga/taiga_project.dart' as _i7;
-import 'protocol/user.dart' as _i8;
-import 'package:taiga_consumer_client/src/protocol/protocol/taiga/taiga_project.dart'
-    as _i9;
-import 'package:taiga_consumer_client/src/protocol/protocol/taiga/taiga_jobs.dart'
-    as _i10;
-import 'package:taiga_consumer_client/src/protocol/protocol/Taiga/taiga_job_updates.dart'
-    as _i11;
-export 'protocol/error_enum.dart';
-export 'protocol/exception.dart';
-export 'protocol/taiga/taiga_job_commentaries.dart';
-export 'protocol/taiga/taiga_job_updates.dart';
-export 'protocol/taiga/taiga_jobs.dart';
-export 'protocol/taiga/taiga_project.dart';
-export 'protocol/user.dart';
 export 'client.dart';
 
 class Protocol extends _i1.SerializationManager {
@@ -50,117 +30,16 @@ class Protocol extends _i1.SerializationManager {
     if (customConstructors.containsKey(t)) {
       return customConstructors[t]!(data, this) as T;
     }
-    if (t == _i2.ErrorEnum) {
-      return _i2.ErrorEnum.fromJson(data) as T;
-    }
-    if (t == _i3.MyException) {
-      return _i3.MyException.fromJson(data, this) as T;
-    }
-    if (t == _i4.TaigaJobCommentaries) {
-      return _i4.TaigaJobCommentaries.fromJson(data, this) as T;
-    }
-    if (t == _i5.TaigaJobUpdates) {
-      return _i5.TaigaJobUpdates.fromJson(data, this) as T;
-    }
-    if (t == _i6.TaigaJob) {
-      return _i6.TaigaJob.fromJson(data, this) as T;
-    }
-    if (t == _i7.TaigaProject) {
-      return _i7.TaigaProject.fromJson(data, this) as T;
-    }
-    if (t == _i8.User) {
-      return _i8.User.fromJson(data, this) as T;
-    }
-    if (t == _i1.getType<_i2.ErrorEnum?>()) {
-      return (data != null ? _i2.ErrorEnum.fromJson(data) : null) as T;
-    }
-    if (t == _i1.getType<_i3.MyException?>()) {
-      return (data != null ? _i3.MyException.fromJson(data, this) : null) as T;
-    }
-    if (t == _i1.getType<_i4.TaigaJobCommentaries?>()) {
-      return (data != null
-          ? _i4.TaigaJobCommentaries.fromJson(data, this)
-          : null) as T;
-    }
-    if (t == _i1.getType<_i5.TaigaJobUpdates?>()) {
-      return (data != null ? _i5.TaigaJobUpdates.fromJson(data, this) : null)
-          as T;
-    }
-    if (t == _i1.getType<_i6.TaigaJob?>()) {
-      return (data != null ? _i6.TaigaJob.fromJson(data, this) : null) as T;
-    }
-    if (t == _i1.getType<_i7.TaigaProject?>()) {
-      return (data != null ? _i7.TaigaProject.fromJson(data, this) : null) as T;
-    }
-    if (t == _i1.getType<_i8.User?>()) {
-      return (data != null ? _i8.User.fromJson(data, this) : null) as T;
-    }
-    if (t == List<_i9.TaigaProject>) {
-      return (data as List)
-          .map((e) => deserialize<_i9.TaigaProject>(e))
-          .toList() as dynamic;
-    }
-    if (t == List<_i10.TaigaJob>) {
-      return (data as List).map((e) => deserialize<_i10.TaigaJob>(e)).toList()
-          as dynamic;
-    }
-    if (t == List<_i11.TaigaJobUpdates>) {
-      return (data as List)
-          .map((e) => deserialize<_i11.TaigaJobUpdates>(e))
-          .toList() as dynamic;
-    }
     return super.deserialize<T>(data, t);
   }
 
   @override
   String? getClassNameForObject(Object data) {
-    if (data is _i2.ErrorEnum) {
-      return 'ErrorEnum';
-    }
-    if (data is _i3.MyException) {
-      return 'MyException';
-    }
-    if (data is _i4.TaigaJobCommentaries) {
-      return 'TaigaJobCommentaries';
-    }
-    if (data is _i5.TaigaJobUpdates) {
-      return 'TaigaJobUpdates';
-    }
-    if (data is _i6.TaigaJob) {
-      return 'TaigaJob';
-    }
-    if (data is _i7.TaigaProject) {
-      return 'TaigaProject';
-    }
-    if (data is _i8.User) {
-      return 'User';
-    }
     return super.getClassNameForObject(data);
   }
 
   @override
   dynamic deserializeByClassName(Map<String, dynamic> data) {
-    if (data['className'] == 'ErrorEnum') {
-      return deserialize<_i2.ErrorEnum>(data['data']);
-    }
-    if (data['className'] == 'MyException') {
-      return deserialize<_i3.MyException>(data['data']);
-    }
-    if (data['className'] == 'TaigaJobCommentaries') {
-      return deserialize<_i4.TaigaJobCommentaries>(data['data']);
-    }
-    if (data['className'] == 'TaigaJobUpdates') {
-      return deserialize<_i5.TaigaJobUpdates>(data['data']);
-    }
-    if (data['className'] == 'TaigaJob') {
-      return deserialize<_i6.TaigaJob>(data['data']);
-    }
-    if (data['className'] == 'TaigaProject') {
-      return deserialize<_i7.TaigaProject>(data['data']);
-    }
-    if (data['className'] == 'User') {
-      return deserialize<_i8.User>(data['data']);
-    }
     return super.deserializeByClassName(data);
   }
 }
--- taiga_consumer_server/lib/src/endpoints/project_endpoint.dart
+++ taiga_consumer_server/lib/src/endpoints/project_endpoint.dart
@@ -1,93 +1,93 @@
-import 'package:serverpod/serverpod.dart';
-import 'package:taiga_consumer_server/src/generated/protocol/taiga/taiga_project.dart';
+// import 'package:serverpod/serverpod.dart';
+// import 'package:taiga_consumer_server/src/generated/protocol/taiga/taiga_project.dart';
 
-// TODO(Nacho): Comentar todo el codigo, ver como handlear los prints
+// // TODO(Nacho): Comentar todo el codigo, ver como handlear los prints
 
-class TaigaProjectEndpoint extends Endpoint {
-  Future<bool> create(Session session, TaigaProject taigaProject) async {
-    try {
-      var response = await TaigaProject.db.insertRow(session, taigaProject);
-      print(response);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
+// class TaigaProjectEndpoint extends Endpoint {
+//   Future<bool> create(Session session, TaigaProject taigaProject) async {
+//     try {
+//       var response = await TaigaProject.db.insertRow(session, taigaProject);
+//       print(response);
+//       return true;
+//     } catch (e) {
+//       return false;
+//     }
+//   }
 
-  Future<bool> createOnBulk(Session session, List<TaigaProject> taigaProject) async {
-    try {
-      var response = await TaigaProject.db.insert(session, taigaProject);
-      print(response);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
+//   Future<bool> createOnBulk(Session session, List<TaigaProject> taigaProject) async {
+//     try {
+//       var response = await TaigaProject.db.insert(session, taigaProject);
+//       print(response);
+//       return true;
+//     } catch (e) {
+//       return false;
+//     }
+//   }
 
-  Future<TaigaProject?> readById(Session session, int id) async {
-    try {
-      var response = await TaigaProject.db.findById(session, id);
-      print(response);
-      return response;
-    } catch (e) {
-      return null;
-    }
-  }
+//   Future<TaigaProject?> readById(Session session, int id) async {
+//     try {
+//       var response = await TaigaProject.db.findById(session, id);
+//       print(response);
+//       return response;
+//     } catch (e) {
+//       return null;
+//     }
+//   }
 
-  Future<TaigaProject?> readByTitle(Session session, TaigaProject taigaProject) async {
-    try {
-      var response = await TaigaProject.db.findFirstRow(
-        session,
-        where: (t) => t.title.equals(taigaProject.title),
-      );
-      print(response);
-      return response;
-    } catch (e) {
-      return null;
-    }
-  }
+//   Future<TaigaProject?> readByTitle(Session session, TaigaProject taigaProject) async {
+//     try {
+//       var response = await TaigaProject.db.findFirstRow(
+//         session,
+//         where: (t) => t.title.equals(taigaProject.title),
+//       );
+//       print(response);
+//       return response;
+//     } catch (e) {
+//       return null;
+//     }
+//   }
 
-  Future<TaigaProject?> readByTaigaProjectId(Session session, int id) async {
-    try {
-      var response = await TaigaProject.db.findFirstRow(
-        session,
-        where: (t) => t.taigaId.equals(id),
-      );
-      print(response);
-      return response;
-    } catch (e) {
-      return null;
-    }
-  }
+//   Future<TaigaProject?> readByTaigaProjectId(Session session, int id) async {
+//     try {
+//       var response = await TaigaProject.db.findFirstRow(
+//         session,
+//         where: (t) => t.taigaId.equals(id),
+//       );
+//       print(response);
+//       return response;
+//     } catch (e) {
+//       return null;
+//     }
+//   }
 
-  Future<bool> updateById(Session session, TaigaProject taigaProject) async {
-    if (taigaProject.id != null) {
-      var modify = await TaigaProject.db.findById(
-        session,
-        taigaProject.id!,
-      );
-      if (modify != null) {
-        modify.taigaId = taigaProject.taigaId;
-        modify.title = taigaProject.title;
-        modify.title = taigaProject.title;
-        var updatedCompany = await TaigaProject.db.updateRow(
-          session,
-          modify,
-        );
-        print(updatedCompany);
-      }
-    }
-    return false;
-  }
+//   Future<bool> updateById(Session session, TaigaProject taigaProject) async {
+//     if (taigaProject.id != null) {
+//       var modify = await TaigaProject.db.findById(
+//         session,
+//         taigaProject.id!,
+//       );
+//       if (modify != null) {
+//         modify.taigaId = taigaProject.taigaId;
+//         modify.title = taigaProject.title;
+//         modify.title = taigaProject.title;
+//         var updatedCompany = await TaigaProject.db.updateRow(
+//           session,
+//           modify,
+//         );
+//         print(updatedCompany);
+//       }
+//     }
+//     return false;
+//   }
 
-  Future<bool> deleteById(Session session, TaigaProject taigaProject) async {
-    if (taigaProject.id != null) {
-      var findRow = await TaigaProject.db.findById(session, taigaProject.id!);
-      if (findRow != null) {
-        var deletedItemId = await TaigaProject.db.deleteRow(session, findRow);
-        print(deletedItemId);
-      }
-    }
-    return false;
-  }
-}
+//   Future<bool> deleteById(Session session, TaigaProject taigaProject) async {
+//     if (taigaProject.id != null) {
+//       var findRow = await TaigaProject.db.findById(session, taigaProject.id!);
+//       if (findRow != null) {
+//         var deletedItemId = await TaigaProject.db.deleteRow(session, findRow);
+//         print(deletedItemId);
+//       }
+//     }
+//     return false;
+//   }
+// }
