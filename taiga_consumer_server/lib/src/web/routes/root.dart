4e25fefd9ce523eb9b377be2c551cf88cf174e73
Ignacio Cardozo <club_dog2@hotmail.com>
2024-01-26 16:43:25 -0300

    Fixed cron to send a resume message every 30m

--- taiga_consumer_server/lib/src/web/routes/root.dart
+++ taiga_consumer_server/lib/src/web/routes/root.dart
@@ -55,20 +55,35 @@ class RouteRoot extends WidgetRoute {
         printData.fromProject.projectId,
       );
 
-      // Will send a message to my mail every 5 minutes)? 
+      // Will send a message to my mail every 5 minutes)?
       if (getProjectById != null) {
         senderCron(session: session, project: getProjectById, minutes: [
-          10,
-          15,
-          20,
-          25,
           30,
-          35,
-          40,
-          45,
-          50,
-          55,
-          60,
+        ], hours: [
+          1,
+          2,
+          3,
+          4,
+          5,
+          6,
+          7,
+          8,
+          9,
+          10,
+          11,
+          12,
+          13,
+          14,
+          15,
+          16,
+          17,
+          18,
+          19,
+          20,
+          21,
+          22,
+          23,
+          24
         ]);
       }
 
